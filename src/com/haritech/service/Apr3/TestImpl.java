8206dc2792d95b35d23246063e3f91160de173ca
narendra <narendra.abburi@perfios.com>
2023-04-06 09:16:43 +0530

    yesterday session

--- src/com/haritech/service/Apr3/TestImpl.java
+++ src/com/haritech/service/Apr3/TestImpl.java
@@ -2,7 +2,7 @@ package com.haritech.service.Apr3;
 
 import com.haritech.service.Apr4.Test;
 
-public class TestImpl implements Test {
+public abstract class TestImpl implements Test {
     public int score(){
         return 10;
     }
